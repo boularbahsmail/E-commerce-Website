1bb06556b022749c3469a3c91415caa312d81527
Ismailium <qiranorane55@gmail.com>
2023-09-22 19:13:48 +0100

    Choose Fashion Category :D

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Icon } from '../Shared/Icon';
+import { fashions_types } from '@/data/FashionsType';
 
 interface menuState {
     menuOpen: boolean;
@@ -8,6 +9,7 @@ interface menuState {
 }
 
 const Header = ({ menuOpen, setMenuOpen }: menuState) => {
+    const [currentCategory, setCurrentCategory] = useState({ open: false, value: "ALL", title: "All Categories" });
     return (
         <header className='max-w-[1400px] w-full mx-auto flex justify-between items-center gap-8'>
             <div className='z-50' style={{ zIndex: 9999 }}>
@@ -20,15 +22,45 @@ const Header = ({ menuOpen, setMenuOpen }: menuState) => {
 
             <div className={`w-full flex lg:flex-row flex-col justify-center items-center lg:gap-10 gap-8 lg:bg-transparent || duration-300 ease-in-out || bg-white lg:static lg:h-auto lg:p-0 || z-50 absolute ${menuOpen ? "left-0" : "-left-full"} top-0 h-screen p-4`}>
                 <div className='lg:max-w-[700px] max-w-[400px] w-full'>
-                    <div className='w-full mx-auto bg-gray-50 border border-gray-100 rounded-[8px] flex lg:flex-row flex-col justify-center items-center lg:gap-4 gap-0 px-4 lg:py-0 py-2'>
-                        <button className='lg:w-[25%] w-full text-sm text-gray-600 flex justify-center items-center gap-2 font-medium px-2 py-3'>
-                            <span>All Categories</span>
-                            <span><Icon title='FiChevronDown' props={{ fontSize: 18 }} /></span>
+                    <div className='relative w-full mx-auto bg-gray-50 border border-gray-100 rounded-[8px] flex lg:flex-row flex-col justify-center items-center lg:gap-4 gap-0 px-4 lg:py-0 py-2'>
+                        <button
+                            className='lg:w-[25%] w-full text-sm text-gray-600 flex justify-center items-center gap-2 font-medium px-2 py-3'
+                            onClick={() => setCurrentCategory({
+                                open: !currentCategory?.open,
+                                value: currentCategory?.value,
+                                title: currentCategory?.title
+                            })}>
+                            <span>{currentCategory?.title}</span>
+                            <span><Icon title={currentCategory?.open ? "FiChevronUp" : "FiChevronDown"} props={{ fontSize: 18 }} /></span>
                         </button>
                         <div className='py-3 lg:w-[75%] w-full flex lg:justify-start justify-center items-center gap-2'>
-                            <span className='text-gray-600'><Icon title='FiSearch' /></span>
+                            <span className='text-gray-600'>
+                                <Icon title='FiSearch' />
+                            </span>
                             <input type='search' placeholder='Search your product...' className='lg:w-full px-2 bg-transparent outline-none font-med text-sm' />
                         </div>
+
+                        {/* Categories */}
+                        {currentCategory.open && <div className='lg:w-[25%] w-full bg-gray-50 border border-gray-100 rounded-[8px] absolute -left-0 top-full mt-2 flex flex-col justify-center items-start overflow-hidden'>
+                            {fashions_types?.map((FashionType: any) => (
+                                <button
+                                    className={`w-full text-[14px] 
+                                    ${currentCategory?.value == FashionType?.key ?
+                                            "font-semibold text-black" :
+                                            "font-normal text-black/70"
+                                        } flex justify-center items-center gap-2 py-3.5 px-4 duration-300 ease-in-out hover:bg-white`}
+                                    onClick={() =>
+                                        setCurrentCategory({
+                                            open: false,
+                                            value: FashionType?.key,
+                                            title: FashionType?.title
+                                        })}
+                                    key={FashionType?.id}
+                                >
+                                    <span>{FashionType?.title}</span>
+                                </button>
+                            ))}
+                        </div>}
                     </div>
                 </div>
 
---
+++ data/FashionsType.ts
@@ -0,0 +1,22 @@
+export const fashions_types = [
+    {
+        id: 1,
+        title: "All Categories",
+        key: "ALL"
+    },
+    {
+        id: 2,
+        title: "Men Fashion",
+        key: "MEN"
+    },
+    {
+        id: 3,
+        title: "Women Fashion",
+        key: "WOMEN"
+    },
+    {
+        id: 4,
+        title: "Kids Fashion",
+        key: "KIDS"
+    }
+];
